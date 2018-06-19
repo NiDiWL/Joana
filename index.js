bae4181a1928825f3c348edbb06493aa222ec88e
Willyam Luccas <willyamluccas@gmail.com>
2018-06-19 02:25:09 -0300

    adicionado const guild e role

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { members } = require('./lista_de_membros.json');
+const GUILD_ID = '439309351698694144'
+const ROLE_ID = '439317715895844876'
 client.on('ready', () => {
 
   client.user.setActivity('o choro dos inimigos', {type: 2}); //type 1 = jogando,   type 2 = ouvindo, type 3 = assistindo
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(`Logged in as ${client.guilds.get('439309351698694144').members.get('157671599124774912').displayName}!`);
-  console.log(`Logged in as ${client.guilds.get('439309351698694144').members[0]}!`);
+  console.log(`Logged in as ${client.guilds.get(GUILD_ID).members.get('157671599124774912').displayName}!`);
+  console.log(`Logged in as ${client.guilds.get(GUILD_ID).members[0]}!`);
   // for (var member in client.guilds.get('439309351698694144').members){
   //   console.log('x')
   // }
   // for (i = 0; i < cars.length; i++) {
   //   text += cars[i] + "<br>";
   // }
-  client.guilds.get('439309351698694144').members.map(function(guildMember) {
+  client.guilds.get(GUILD_ID).members.map(function(guildMember) {
     if (members.some(function(member) { return guildMember.displayName.indexOf(member) > -1; })) {
       console.log (`É membro: ${guildMember.displayName}`);
     } else {
