7c9d8bda0403b272f02665f293d9aa10effb378a
Willyam Luccas <willyamluccas@gmail.com>
2018-06-19 02:18:27 -0300

    Listado os membro no log

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const { members } = require('./lista_de_membros.json');
 client.on('ready', () => {
 
-  client.user.setActivity("o choro dos inimigos", {type: 2}); //type 1 = jogando,   type 2 = ouvindo, type 3 = assistindo
+  client.user.setActivity('o choro dos inimigos', {type: 2}); //type 1 = jogando,   type 2 = ouvindo, type 3 = assistindo
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.guilds.get('439309351698694144').members.get('157671599124774912').displayName}!`);
+  console.log(`Logged in as ${client.guilds.get('439309351698694144').members[0]}!`);
+  // for (var member in client.guilds.get('439309351698694144').members){
+  //   console.log('x')
+  // }
+  // for (i = 0; i < cars.length; i++) {
+  //   text += cars[i] + "<br>";
+  // }
+  client.guilds.get('439309351698694144').members.map(function(guildMember) {
+    if (members.some(function(member) { return guildMember.displayName.indexOf(member) > -1; })) {
+      console.log (`É membro: ${guildMember.displayName}`);
+    } else {
+      console.log (`Não é membro: ${guildMember.displayName}`);
+    }
+  });
 });
 
 client.on('message', msg => {
